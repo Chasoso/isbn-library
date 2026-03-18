38be3f71bd1a49a99a9ebcd5c7eb7b3bd0236e60
Chasoso <hideaki.yamamoto23@gmail.com>
2026-03-18 20:15:37 +0900

    fix a bug in auto test

--- backend/lambda/shared/python/shared/dynamo.py
+++ backend/lambda/shared/python/shared/dynamo.py
@@ -1,13 +1,15 @@
 import os
+from typing import Any
 
 import boto3
 
 from .constants import BOOKS_TABLE_NAME_ENV
 
 
-_resource = boto3.resource("dynamodb")
+def get_dynamodb_resource() -> Any:
+    return boto3.resource("dynamodb")
 
 
-def get_books_table():
+def get_books_table() -> Any:
     table_name = os.environ[BOOKS_TABLE_NAME_ENV]
-    return _resource.Table(table_name)
+    return get_dynamodb_resource().Table(table_name)
